0a8b2841f4a90f61512e690311eda786f8cbf586
Rody <rody.van.dijk@gmail.com>
2022-01-14 14:14:59 +0100

    Changes

--- style.js
+++ style.js
@@ -6,10 +6,23 @@ function closeNav() {
   document.getElementById("slideNav").style.width = "0";
 }
 
-function dark() {
-  document.cookie = theme + "=" + dark;
+// functions om theme te zetten
+function setTheme(themeName) {
+  localStorage.setItem("theme", themeName);
+  document.documentElement.className = themeName;
 }
-
-function light() {
-  document.cookie = theme + "=" + light;
+// functions om tussen themes te switchen
+function setThemeNeonDark() {
+  setTheme("themeNeonDark");
 }
+function setThemeNeonLight() {
+  setTheme("themeNeonLight");
+}
+// tijdens laden van pagina moet ik het gekozen thema gebruiken
+(function () {
+  if (localStorage.getItem("theme") === "themeNeonDark") {
+    setTheme("themeNeonDark");
+  } else if (localStorage.getItem("theme") === "themeNeonLight") {
+    setTheme("themeNeonLight");
+  }
+})();
